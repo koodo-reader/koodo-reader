c91d8c0ba590b46c725bdaa73b6f22ba06fb0e8f
troyeguo <13820674+troyeguo@users.noreply.github.com>
2020-03-04 12:35:40 +0800

    test

---
+++ .github/ISSUE_TEMPLATE.md
@@ -0,0 +1,4 @@
+<!-- 
+  Is this a bug report?
+  If so, go back and select the "Bug report" option or your issue WILL be closed.
+--!>
---
+++ app/actions/counter.ts
@@ -0,0 +1,36 @@
+import { GetState, Dispatch } from '../reducers/types';
+
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+
+export function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  };
+}
+
+export function decrement() {
+  return {
+    type: DECREMENT_COUNTER
+  };
+}
+
+export function incrementIfOdd() {
+  return (dispatch: Dispatch, getState: GetState) => {
+    const { counter } = getState();
+
+    if (counter % 2 === 0) {
+      return;
+    }
+
+    dispatch(increment());
+  };
+}
+
+export function incrementAsync(delay = 1000) {
+  return (dispatch: Dispatch) => {
+    setTimeout(() => {
+      dispatch(increment());
+    }, delay);
+  };
+}
