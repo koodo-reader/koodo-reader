61186da7305788d96d9fa151ee02ee3d2bb1640a
troyeguo <guo362429@163.com>
2020-06-21 11:57:52 +0800

    bug修复

---
+++ src/pages/manager/interface.tsx
@@ -0,0 +1,36 @@
+import BookModel from "../../model/Book";
+import NoteModel from "../../model/Note";
+import DigestModel from "../../model/Digest";
+import BookmarkModel from "../../model/Bookmark";
+export interface ManagerProps {
+  books: BookModel[];
+  covers: { key: string; url: string }[];
+  notes: NoteModel[];
+  digests: DigestModel[];
+  bookmarks: BookmarkModel[];
+  isReading: boolean;
+  mode: string;
+  shelfIndex: number;
+  isOpenEditDialog: boolean;
+  isOpenDeleteDialog: boolean;
+  isOpenAddDialog: boolean;
+  isSort: boolean;
+  isFirst: string;
+  isSortDisplay: boolean;
+  isMessage: boolean;
+  isBackup: boolean;
+  handleFetchBooks: () => void;
+  handleFetchNotes: () => void;
+  handleFetchDigests: () => void;
+  handleFetchBookmarks: () => void;
+  handleFetchHighlighters: () => void;
+  handleFetchSortCode: () => void;
+  handleFetchList: () => void;
+  handleMessageBox: (isShow: boolean) => void;
+  handleFirst: (isFirst: string) => void;
+}
+
+export interface ManagerState {
+  totalBooks: number;
+  recentBooks: number;
+}
---
+++ src/pages/reader/interface.tsx
@@ -0,0 +1,21 @@
+import BookModel from "../../model/Book";
+export interface ReaderProps {
+  currentEpub: any;
+  currentBook: BookModel;
+  isMessage: boolean;
+  handleFetchNotes: () => void;
+  handleFetchDigests: () => void;
+  handleFetchBookmarks: () => void;
+  handleFetchHighlighters: () => void;
+  handleMessageBox: (isShow: boolean) => void;
+  handleFetchPercentage: (currentBook: BookModel) => void;
+  handleFetchChapters: (currentEpub: any) => void;
+}
+
+export interface ReaderState {
+  isOpenSettingPanel: boolean;
+  isOpenOperationPanel: boolean;
+  isOpenProgressPanel: boolean;
+  isOpenInfoPanel: boolean;
+  isMessage: boolean;
+}
