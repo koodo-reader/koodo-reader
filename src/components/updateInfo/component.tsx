3552c0c5ab80528c3e27260b625914b4fb9b78f5
troyeguo <13820674+troyeguo@users.noreply.github.com>
2020-09-11 17:18:57 +0800

    fix bug
    Former-commit-id: 447285cb97e3c2e40fdf9d7523d5447e5c602e12

--- src/components/updateInfo/component.tsx
+++ src/components/updateInfo/component.tsx
@@ -19,7 +19,6 @@ class UpdateInfo extends React.Component<UpdateInfoProps, UpdateInfoState> {
         console.log(res);
         const download = res.data.download;
         const version = res.data.log.version;
-        // console.log(version, "version");
         if (this.compareVersion(updateLog.version, version)) {
           navigator.platform === "Win32"
             ? this.setState({ downlownLink: download[0].url })
