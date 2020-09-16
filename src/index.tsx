bc594490cd1b2de1ad1034bcdbbe41f88145476b
troyeguo <13820674+troyeguo@users.noreply.github.com>
2020-09-16 09:36:55 +0800

    Update zh-TW translation
    Former-commit-id: ec8c47a3e3b1c9eada67441514bb106bf0d2a640

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
