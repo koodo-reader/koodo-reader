3d6a1b45715888109030e87c8a0a3b4eb381c886
troyeguo <13820674+troyeguo@users.noreply.github.com>
2020-10-18 09:55:15 +0800

    fix bug
    Former-commit-id: a87e1630b18807a100e622de079873c180b16a5c

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,19 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
-import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import { Route, Switch, Redirect, HashRouter } from "react-router-dom";
 import Manager from "../pages/manager";
 import EpubReader from "../pages/epubReader";
 import PdfReader from "../pages/pdfReader";
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route component={Manager} path="/manager" />
         <Route component={EpubReader} path="/epub" />
         <Route component={PdfReader} path="/pdf" />
         <Redirect to="/manager/home" />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
