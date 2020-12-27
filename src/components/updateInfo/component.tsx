61f9a121a935ecc8b50d201dc028c7d7f43a7fb3
troyeguo <guo362429@163.com>
2020-12-27 16:02:33 +0800

    Update to 1.1.9

--- src/components/updateInfo/component.tsx
+++ src/components/updateInfo/component.tsx
@@ -23,9 +23,11 @@ class UpdateInfo extends React.Component<UpdateInfoProps, UpdateInfoState> {
           const download = res.data.download;
           const version = res.data.log.version;
           if (this.compareVersion(updateLog.version, version)) {
-            navigator.platform === "Win32"
-              ? this.setState({ downlownLink: download[0].url })
-              : this.setState({ downlownLink: download[1].url });
+            navigator.platform.indexOf("Linux") > -1
+              ? this.setState({ downlownLink: download[2].url })
+              : navigator.platform.indexOf("Mac") > -1
+              ? this.setState({ downlownLink: download[1].url })
+              : this.setState({ downlownLink: download[0].url });
           }
         })
         .catch((err) => {
