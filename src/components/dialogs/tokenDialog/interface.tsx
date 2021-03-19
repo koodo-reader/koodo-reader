a620ae12aae3b425bf4a851b96431be77e710f10
troyeguo <13820674+troyeguo@users.noreply.github.com>
2021-03-19 20:01:01 +0800

    fix bug
    Former-commit-id: 1d7e296491a8755ab4ab0c0c22d0ff25520ea696

--- src/components/dialogs/tokenDialog/interface.tsx
+++ src/components/dialogs/tokenDialog/interface.tsx
@@ -1,4 +1,4 @@
-import BookModel from "../../model/Book";
+import BookModel from "../../../model/Book";
 export interface TokenDialogProps {
   handleTokenDialog: (isShow: boolean) => void;
   currentBook: BookModel;
