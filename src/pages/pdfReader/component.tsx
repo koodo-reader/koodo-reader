28ac172e43a8482046fd18bdbc226b85d9f6ac81
unknown <guo362429@gmail.com>
2021-03-24 21:25:06 +0800

    fix bug

---
+++ src/pages/pdfReader/component.tsx
@@ -0,0 +1,57 @@
+//卡片模式下的图书显示
+import React from "react";
+import RecentBooks from "../../utils/readUtils/recordRecent";
+// import "./bookCardItem.css";
+import { PdfReaderProps, PdfReaderState } from "./interface";
+import localforage from "localforage";
+import Reader from "../../containers/epubViewer";
+import { withRouter } from "react-router-dom";
+import _ from "underscore";
+import BookUtil from "../../utils/bookUtil";
+import Lottie from "react-lottie";
+import animationSiri from "../../assets/lotties/siri.json";
+
+const siriOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationSiri,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+declare var window: any;
+
+class PdfReader extends React.Component<PdfReaderProps, PdfReaderState> {
+  epub: any;
+  constructor(props: PdfReaderProps) {
+    super(props);
+    this.state = {};
+  }
+
+  componentWillMount() {
+    let url = document.location.href.split("/");
+    let key = url[url.length - 1].split("?")[0];
+
+    localforage.getItem("books").then((result: any) => {
+      let book = result[_.findIndex(result, { key })];
+      BookUtil.fetchBook(key).then((result) => {
+        this.props.handleReadingBook(book);
+        this.props.handleReadingEpub(window.ePub(result, {}));
+        this.props.handleReadingState(true);
+        RecentBooks.setRecent(key);
+      });
+    });
+  }
+
+  render() {
+    if (!this.props.isReading) {
+      return (
+        <div className="spinner">
+          <Lottie options={siriOptions} height={100} width={300} />
+        </div>
+      );
+    }
+    return <Reader />;
+  }
+}
+export default withRouter(PdfReader);
