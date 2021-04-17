dd36161a3dd899cc30c275fbcc26e0258735bc28
troyeguo <13820674+troyeguo@users.noreply.github.com>
2021-04-17 22:06:00 +0800

    fix bug
    Former-commit-id: 10a4f738928b554f574035156fcb94252d188779

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,16 @@ import "./i18n";
 import store from "./store";
 import Router from "./router/index";
 import OtherUtil from "./utils/otherUtil";
+const addStyle = (url: string) => {
+  const style = document.createElement("link");
+  style.href = url;
+  style.rel = "stylesheet";
+  document.head.appendChild(style);
+};
 if (OtherUtil.getReaderConfig("isDisplayDark") === "yes") {
-  require("./assets/styles/dark.css");
+  addStyle("/assets/dark.css");
 } else {
-  require("./assets/styles/default.css");
+  addStyle("/assets/default.css");
 }
 
 let coverLoading: any = document.querySelector(".loading-cover");
