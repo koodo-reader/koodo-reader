1000f1c894272ba93c12ca7f3adde237569f7410
troyeguo <13820674+troyeguo@users.noreply.github.com>
2021-04-29 14:29:29 +0800

    fix bug
    Former-commit-id: a28bb0ef18d01505456cb5d5eda4e743c0c4ebd1

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -25,7 +25,7 @@ i18n
   .use(reactI18nextModule as any) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "zh",
+    lng: "en",
     fallbackLng: "en",
     keySeparator: false, // we do not use keys in form messages.welcome
     interpolation: {
