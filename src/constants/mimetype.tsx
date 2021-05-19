7c0ab79643e751043eac605a2b2fe7ba28ad6e75
troyeguo <13820674+troyeguo@users.noreply.github.com>
2021-05-19 18:36:33 +0800

    fix bug
    Former-commit-id: 567f4bae05efb9aa90f1725cadbeaca4afc92fea

---
+++ src/constants/mimetype.tsx
@@ -0,0 +1,8 @@
+export const mimetype = {
+  svg: "image/svg+xml",
+  png: "image/png",
+  jpg: "image/jpeg",
+  jpeg: "image/jpeg",
+  gif: "image/gif",
+  webp: "image/webp",
+};
--- src/pages/comicReader/component.tsx
+++ src/pages/comicReader/component.tsx
@@ -6,11 +6,15 @@ import localforage from "localforage";
 import { withRouter } from "react-router-dom";
 import _ from "underscore";
 import BookUtil from "../../utils/bookUtil";
-// import { Unzipper } from "@codedread/bitjs";
 import "./viewer.css";
+import untar from "js-untar";
 import OtherUtil from "../../utils/otherUtil";
+import { mimetype } from "../../constants/mimetype";
 
 declare var window: any;
+
+let JSZip = (window as any).JSZip;
+let Unrar = (window as any).Unrar;
 
 class Viewer extends React.Component<ViewerProps, ViewerState> {
   epub: any;
@@ -54,26 +58,98 @@ class Viewer extends React.Component<ViewerProps, ViewerState> {
     OtherUtil.setReaderConfig("windowX", window.screenX + "");
     OtherUtil.setReaderConfig("windowY", window.screenY + "");
   }
-  handleCbz = (result: ArrayBuffer) => {
-    // const unzipper = new Unzipper(result);
-    // unzipper.addEventListener("progress", (e) => {
-    //   console.log(e.currentFilename, e.totalCompressedBytesRead);
-    // });
-    // unzipper.addEventListener("extract", (e) => {
-    //   console.log(e.unarchivedFile.filename, e.unarchivedFile.fileData);
-    // });
-    // unzipper.addEventListener("finish", () => {
-    //   console.log("finish");
-    // });
-    // unzipper.start();
-    // let viewer: HTMLElement | null = document.querySelector(".ebook-viewer");
-    // if (!viewer?.innerHTML) return;
-    // viewer.innerHTML = "";
+  base64ArrayBuffer = (arrayBuffer) => {
+    var base64 = "";
+    var encodings =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    var bytes = new Uint8Array(arrayBuffer);
+    var byteLength = bytes.byteLength;
+    var byteRemainder = byteLength % 3;
+    var mainLength = byteLength - byteRemainder;
+    var a, b, c, d;
+    var chunk;
+    for (var i = 0; i < mainLength; i = i + 3) {
+      chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+      a = (chunk & 16515072) >> 18;
+      b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
+      c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
+      d = chunk & 63; // 63       = 2^6 - 1
+      base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
+    }
+
+    if (byteRemainder === 1) {
+      chunk = bytes[mainLength];
+
+      a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
+      b = (chunk & 3) << 4; // 3   = 2^2 - 1
+      base64 += encodings[a] + encodings[b] + "==";
+    } else if (byteRemainder === 2) {
+      chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+      a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
+      b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
+      c = (chunk & 15) << 2; // 15    = 2^4 - 1
+      base64 += encodings[a] + encodings[b] + encodings[c] + "=";
+    }
+
+    return base64;
   };
-  handleCbr = (result: ArrayBuffer) => {};
-  handleCbt = (result: ArrayBuffer) => {};
+  addImage = (content: ArrayBuffer, extension: string) => {
+    let url = this.base64ArrayBuffer(content);
+
+    let imageDom = document.createElement("img");
+    imageDom.src = "data:" + mimetype[extension] + ";base64," + url;
+    let viewer: HTMLElement | null = document.querySelector(".ebook-viewer");
+    if (!viewer?.innerHTML) return;
+    viewer.appendChild(imageDom);
+    let loading = document.querySelector("p");
+    if (!loading) return;
+    viewer.removeChild(loading);
+  };
+  handleCbz = (result: ArrayBuffer) => {
+    let zip = new JSZip();
+    zip.loadAsync(result).then((contents) => {
+      Object.keys(contents.files).forEach(async (filename) => {
+        const content = await zip.file(filename).async("arraybuffer");
+        const extension = filename.split(".").reverse()[0];
+        this.addImage(content, extension);
+      });
+    });
+  };
+  handleCbr = (result: ArrayBuffer) => {
+    let unrar = new Unrar(result);
+    var entries = unrar.getEntries();
+    for (let item of entries) {
+      var fileData = unrar.decompress(item.name);
+      if (!fileData) {
+        console.log("decompress failed...");
+      }
+      const extension = item.name.split(".").reverse()[0];
+      this.addImage(fileData, extension);
+    }
+  };
+  handleCbt = (result: ArrayBuffer) => {
+    untar(result).then(
+      (extractedFiles) => {
+        for (let item of extractedFiles) {
+          const extension = item.name.split(".").reverse()[0];
+          this.addImage(item.buffer, extension);
+        }
+        // onSuccess
+      },
+      function (err) {
+        // onError
+      },
+      function (extractedFile) {
+        // onProgress
+      }
+    );
+  };
   render() {
-    return <div className="ebook-viewer">Loading</div>;
+    return (
+      <div className="ebook-viewer">
+        <p>Loading</p>
+      </div>
+    );
   }
 }
 export default withRouter(Viewer as any);
