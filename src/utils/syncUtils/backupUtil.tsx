599b78398e015346f60e91a0687601679fcc54bb
troyeguo <guo362429@163.com>
2021-06-14 13:28:38 +0800

    fix bug

--- src/utils/syncUtils/backupUtil.tsx
+++ src/utils/syncUtils/backupUtil.tsx
@@ -20,6 +20,7 @@ class BackupUtil {
     driveIndex: number,
     showMessage: (message: string) => void
   ) => {
+    console.log(3);
     let zip = new JSZip();
     let books = bookArr;
     //0表示备份到本地，1表示备份到dropbox,2表示备份到onedrive,3表示备份到webdav，4表示转移数据，5表示同步数据到本地
@@ -107,6 +108,7 @@ class BackupUtil {
 
             break;
           case 5:
+            console.log(2);
             handleFinish();
             moveData(blob, 5, [], handleFinish);
 
