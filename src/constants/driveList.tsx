686d752030608ea30ff5a6efa75a0806035159ec
troyeguo <13820674+troyeguo@users.noreply.github.com>
2021-06-26 10:50:40 +0800

    fix bug
    Former-commit-id: f2d20dec9148dcd5d0e014f35d6297ee6c2e0d3b

--- src/constants/driveList.tsx
+++ src/constants/driveList.tsx
@@ -7,7 +7,7 @@ export const config = {
     process.env.NODE_ENV === "production"
       ? "http://localhost:3366"
       : "http://localhost:3366",
-  dropbox_client_id: "e3zgg310xbizvaf",
+  dropbox_client_id: "vnc67byrssocvy1",
   googledrive_client_id:
     "99440516227-ifr1ann33f2j610i3ri17ej0i51c7m6e.apps.googleusercontent.com",
   onedrive_client_id: "ac96f9bf-94f2-49c0-8418-999b919bc236",
