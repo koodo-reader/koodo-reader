cbd24e4a00d1e1bfb710007311037ccf8b6e3f28
troyeguo <guo362429@gmail.com>
2021-10-18 20:16:00 +0800

    fix bug

--- src/model/HtmlBook.ts
+++ src/model/HtmlBook.ts
@@ -1,15 +1,17 @@
 class HtmlBook {
-
+  key:string;
   doc:HTMLElement;
   chapters:{label:string,id:string,href:string}[];
   subitems:any;
   chapterDoc:string[];
   constructor(
+    key:string,
     doc:HTMLElement,
     chapters:{label:string,id:string,href:string}[],
     subitems:any,
     chapterDoc:string[],
   ) {
+    this.key=key,
     this.doc=doc,
     this.chapters=chapters,
     this.subitems=subitems,
