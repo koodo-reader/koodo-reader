b6b18f30a2c5d112bca691c423fbfc1e9a51cc95
troyeguo <13820674+troyeguo@users.noreply.github.com>
2021-12-29 12:57:12 +0800

    fix bug
    Former-commit-id: 834bf3505051db495ada33665d0ccae0a28e2259

--- src/utils/fileUtils/xmlUtil.tsx
+++ src/utils/fileUtils/xmlUtil.tsx
@@ -24,7 +24,6 @@ export const xmlBookToObj = (xml) => {
       }
       informBook += "</h3>";
     }
-
     if (fictionBook.binary) {
       objBook.posterSrc =
         "data:image/jpeg;base64," + fictionBook.binary[0]["_"];
@@ -35,11 +34,11 @@ export const xmlBookToObj = (xml) => {
   return informBook;
 };
 
-export const xmlBookTagFilter = (bookString) => {
+export const xmlBookTagFilter = (xml: any, bookString: string) => {
   var regExpTagDelete = /<epigraph>|<\/epigraph>|<empty-line\/>|/gi;
   var regExpTitleOpen = /<title>/gi;
   var regExpTitleClose = /<\/title>/gi;
-  var bookStart = bookString.match(/<body.*?>/i);
+  var bookStart = bookString.match(/<body.*?>/i) || [""];
   var bookBody = bookString.slice(
     bookString.search(/<body.*?>/i) + bookStart[0].length,
     bookString.search(/<\/body>/i)
@@ -49,5 +48,36 @@ export const xmlBookTagFilter = (bookString) => {
   bookBody = bookBody.replace(regExpTitleOpen, "<h3>");
   bookBody = bookBody.replace(regExpTitleClose, "</h3>");
 
-  return bookBody;
+  return xmlImageHandler(xml, bookBody);
+};
+
+export const xmlImageHandler = (xml: any, bookString: string) => {
+  let parser = new xml2js.Parser();
+  parser.parseString(xml, function (err, result) {
+    if (err) {
+      console.log("Error with parsing xml" + err);
+      return "";
+    }
+
+    var fictionBook = result.FictionBook;
+    if (fictionBook.binary) {
+      let strArr = bookString.split(`<image xlink:href="#`);
+
+      for (let i = 0; i < strArr.length; i++) {
+        for (let j = 0; j < fictionBook.binary.length; j++) {
+          if (strArr[i].startsWith(fictionBook.binary[j]["$"].id)) {
+            strArr[i] =
+              '<img alt="poster" src="' +
+              "data:image/jpeg;base64," +
+              fictionBook.binary[j]["_"] +
+              strArr[i].slice(fictionBook.binary[j]["$"].id.length);
+          }
+        }
+      }
+
+      bookString = strArr.join("");
+    }
+  });
+
+  return bookString;
 };
