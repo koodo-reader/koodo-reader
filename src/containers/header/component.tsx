d99a49d061967f381b2fab919eba29cd1a71c22b
troyeguo <13820674+troyeguo@users.noreply.github.com>
2022-01-17 18:31:52 +0800

    fix bug
    Former-commit-id: 4237fc6ef9d244215ee4d50bf484ddf9fd628461

--- src/containers/header/component.tsx
+++ src/containers/header/component.tsx
@@ -212,7 +212,7 @@ class Header extends React.Component<HeaderProps, HeaderState> {
             style={{ left: "490px", top: "18px" }}
           >
             <Tooltip
-              title={this.props.t("Sort")}
+              title={this.props.t("Sort by")}
               position="top"
               trigger="mouseenter"
               distance={20}
@@ -287,7 +287,7 @@ class Header extends React.Component<HeaderProps, HeaderState> {
           <div className="animation-mask"></div>
           {this.props.isCollapsed && this.state.width < 950 ? (
             <Tooltip
-              title={this.props.t("Backup and Restore")}
+              title={this.props.t("Backup")}
               position="top"
               trigger="mouseenter"
             >
@@ -297,7 +297,7 @@ class Header extends React.Component<HeaderProps, HeaderState> {
               ></span>
             </Tooltip>
           ) : (
-            <Trans>Backup and Restore</Trans>
+            <Trans>Backup</Trans>
           )}
         </div>
         <ImportLocal
