2d17cb9d0036f3f920bc1bcc067a664ecb1de712
troyeguo <13820674+troyeguo@users.noreply.github.com>
2023-09-15 08:09:51 +0800

    fix bug
    Former-commit-id: b21c0f7e6693708a97fac0785fe4b8b9d6b4f98d

--- src/constants/popupList.tsx
+++ src/constants/popupList.tsx
@@ -1,10 +1,10 @@
 export const popupList = [
   { name: "note", title: "Take Note", icon: "note" },
   { name: "digest", title: "Highlight", icon: "highlight" },
-  { name: "dict", title: "Dictionary", icon: "dict" },
+  { name: "translation", title: "Translate", icon: "translation" },
   { name: "copy", title: "Copy", icon: "copy" },
   { name: "search-book", title: "Search in the book", icon: "search-book" },
-  { name: "translation", title: "Translate", icon: "translation" },
+  { name: "dict", title: "Dictionary", icon: "dict" },
   { name: "browser", title: "Search on the internet", icon: "browser" },
   { name: "speaker", title: "Speak the text", icon: "Speaker" },
 ];
