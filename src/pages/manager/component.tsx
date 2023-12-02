f358e51317dcd8355bc086c5d6bf7b032382d5dd
troyeguo <13820674+troyeguo@users.noreply.github.com>
2023-12-02 09:26:09 +0800

    fix bug
    Former-commit-id: e37c7ff90d28b733f1e9ff6f9c0345044acdc328

--- src/pages/manager/component.tsx
+++ src/pages/manager/component.tsx
@@ -78,35 +78,6 @@ class Manager extends React.Component<ManagerProps, ManagerState> {
   };
   render() {
     let { books } = this.props;
-    if (isMobile && document.location.href.indexOf("192.168") === -1) {
-      return (
-        <>
-          <p className="waring-title">
-            <Trans>Warning</Trans>
-          </p>
-          <div className="mobile-warning">
-            <span>
-              <Trans>
-                For better user experince, please visit this site on a computer
-              </Trans>
-            </span>
-          </div>
-          <div>
-            <img
-              src={
-                StorageUtil.getReaderConfig("appSkin") === "night" ||
-                (StorageUtil.getReaderConfig("appSkin") === "system" &&
-                  StorageUtil.getReaderConfig("isOSNight") === "yes")
-                  ? "./assets/empty_light.svg"
-                  : "./assets/empty.svg"
-              }
-              alt=""
-              className="waring-pic"
-            />
-          </div>
-        </>
-      );
-    }
     return (
       <div
         className="manager"
