8396a290447ec34f9305964139d1125b92167aec
troyeguo <13820674+troyeguo@users.noreply.github.com>
2024-01-04 09:06:24 +0800

    add missing terms for i18n
    Former-commit-id: d69be20bc8a6d12ea7c005ee26c3a98a34eb7739

---
+++ src/i18n-script.js
@@ -0,0 +1,58 @@
+// the translations
+const resources = [
+  "./assets/locales/zh-TW/translation.json",
+  "./assets/locales/zh-MO/translation.json",
+  "./assets/locales/ar/translation.json",
+  "./assets/locales/tr/translation.json",
+  "./assets/locales/ro/translation.json",
+  "./assets/locales/pl/translation.json",
+  "./assets/locales/cs/translation.json",
+  "./assets/locales/jp/translation.json",
+  "./assets/locales/bo/translation.json",
+  "./assets/locales/id/translation.json",
+  "./assets/locales/hy/translation.json",
+  "./assets/locales/bg/translation.json",
+  "./assets/locales/it/translation.json",
+  "./assets/locales/bn/translation.json",
+  "./assets/locales/nl/translation.json",
+  "./assets/locales/ko/translation.json",
+  "./assets/locales/de/translation.json",
+  "./assets/locales/ru/translation.json",
+  "./assets/locales/fr/translation.json",
+  "./assets/locales/es/translation.json",
+  "./assets/locales/fa/translation.json",
+  "./assets/locales/pt-BR/translation.json",
+  "./assets/locales/th/translation.json",
+];
+const fs = require("fs");
+const path = require("path");
+const data = fs.readFileSync(
+  path.join(__dirname, "./assets/locales/en/translation.json"),
+  "utf-8"
+);
+const referData = JSON.parse(data);
+for (let index = 0; index < resources.length; index++) {
+  try {
+    const resource = resources[index];
+    console.log(path.join(__dirname, resource));
+    const data = fs.readFileSync(path.join(__dirname, resource), "utf-8");
+    const targetData = JSON.parse(data);
+    let missingTerms = {};
+    for (let index = 0; index < Object.keys(referData).length; index++) {
+      const term = Object.keys(referData)[index];
+      if (Object.keys(targetData).indexOf(term) === -1) {
+        console.log(referData[term]);
+        missingTerms[term] = referData[term];
+      }
+    }
+    // console.log(missingTerms);
+    const mergedObj = Object.assign({}, targetData, missingTerms);
+
+    fs.writeFileSync(
+      path.join(__dirname, resource),
+      JSON.stringify(mergedObj, null, 2)
+    );
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+}
