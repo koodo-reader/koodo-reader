cb3dcca0a0abf44ad3d1e72049b55c7c3a79beb9
troyeguo <13820674+troyeguo@users.noreply.github.com>
2024-01-07 13:33:51 +0800

    test upload script
    Former-commit-id: e1ae5c03ef2b9caf34121477885ec53a2901cd76

---
+++ src/upload copy.sh
@@ -0,0 +1,23 @@
+
+# Create a directory with the name of the tag
+mkdir -p $TAG
+
+# Get the release details from GitHub API
+RELEASE=$(curl --silent "https://api.github.com/repos/$USER/$REPO/releases/tags/$TAG")
+
+# Get the assets from the release
+ASSETS=$(echo $RELEASE | jq -r '.assets[] | .browser_download_url')
+
+# Download each asset
+for ASSET in $ASSETS; do
+    curl -L -o $TAG/$(basename $ASSET) $ASSET
+done
+
+# Install B2 CLI
+wget https://github.com/Backblaze/B2_Command_Line_Tool/releases/latest/download/b2-linux
+mv b2-linux b2
+chmod +x ./b2
+./b2 authorize-account $B2_ACCOUNT_ID $B2_APPLICATION_KEY
+
+# Upload the directory to Backblaze
+./b2 sync --replaceNewer $TAG b2://$BUCKET/$TAG
