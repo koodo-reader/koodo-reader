fa81b9b47695d0a768b9312b8b3f007005db37a2
troyeguo <guo362429@gmail.com>
2024-01-07 13:33:51 +0800

    test upload script

--- src/upload.sh
+++ src/upload.sh
@@ -3,7 +3,7 @@
 # GitHub repo details
 USER="koodo-reader"
 REPO="koodo-reader"
-TAG="v1.5.1"
+TAG="v1.6.0"
 
 # Backblaze details
 BUCKET="koodo-reader"
@@ -33,3 +33,56 @@ chmod +x ./b2
 
 # Upload the directory to Backblaze
 ./b2 sync --replaceNewer $TAG b2://$BUCKET/$TAG
+
+# 获取文件列表
+file_list=$(./b2 ls --long "$BUCKET" | grep -oE '^.*/$')
+
+# 创建一个新的HTML文件
+html_file="file_list.html"
+echo "<html><body><table>" > $html_file
+
+# 添加表头
+echo "<tr><th>File Name</th><th>File Size</th><th>Last Modified</th></tr>" >> $html_file
+
+# 遍历文件列表
+echo "$file_list" | while read line; do
+    # 获取文件名、文件类型和最后修改时间
+    file_name=$(echo $line | awk '{print $6}')
+    file_size=$(echo $line | awk '{print $5}')
+    last_modified=$(echo $line | awk '{print $3}')
+
+    # 添加到HTML文件
+    echo "<tr><td><a href="./$(echo "$file_name" | sed 's/\///g').html">$(echo "$file_name" | sed 's/\///g')</a></td><td>$file_size</td><td>$last_modified</td></tr>" >> $html_file
+done
+
+# 结束HTML文件
+echo "</table></body></html>" >> $html_file
+
+./b2 upload-file $BUCKET file_list.html index.html
+
+# 获取文件列表
+file_list=$(./b2 ls --long "$BUCKET" "$TAG")
+
+# 创建一个新的HTML文件
+html_file="file_list.html"
+echo "<html><body><table>" > $html_file
+
+# 添加表头
+echo "<tr><th>File Name</th><th style='width:100px'>File Size</th><th style='width:100px'>Last Modified</th></tr>" >> $html_file
+
+# 遍历文件列表
+echo "$file_list" | while read line; do
+    # 获取文件名、文件类型和最后修改时间
+    file_name=$(echo $line | awk '{print $6}')
+    file_size=$(echo $line | awk '{print $5}')
+    last_modified=$(echo $line | awk '{print $3}')
+
+    # 添加到HTML文件
+    echo "<tr><td><a href="./$file_name">$(echo "$file_name" | sed 's/.*\///')</a></td><td style='width:100px'>$(($((file_size)) / 1024 / 1024)) MB</td><td style='width:100px'>$last_modified</td></tr>" >> $html_file
+done
+
+# 结束HTML文件
+echo "</table></body></html>" >> $html_file
+
+./b2 upload-file $BUCKET file_list.html $TAG.html
+
