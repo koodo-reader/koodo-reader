0b75ea29c670296769a077ad10d33ed4ed6fb7b3
troyeguo <13820674+troyeguo@users.noreply.github.com>
2024-01-12 20:12:47 +0800

    fix bug
    Former-commit-id: 232f91ea7de7b4d5c2bcc240398b21cfa8f7e1c5

--- src/pages/manager/interface.tsx
+++ src/pages/manager/interface.tsx
@@ -35,6 +35,7 @@ export interface ManagerProps extends RouteComponentProps<any> {
   handleAddDialog: (isOpenAddDialog: boolean) => void;
   handleTipDialog: (isTipDialog: boolean) => void;
   handleDetailDialog: (isDetailDialog: boolean) => void;
+  handleFeedbackDialog: (isShow: boolean) => void;
   handleLoadingDialog: (isShowLoading: boolean) => void;
   handleNewDialog: (isShowNew: boolean) => void;
   handleBackupDialog: (isBackup: boolean) => void;
