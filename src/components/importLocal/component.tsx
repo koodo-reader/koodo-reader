3078659ace98de38434774b9650e80de4baac00d
troyeguo <13820674+troyeguo@users.noreply.github.com>
2026-02-12 18:04:33 +0800

    Refactor importLocal component and add i18n mobile script for localization management
    - Removed unused imports in the importLocal component.
    - Added a new script to manage localization files, including:
      - Listing folders in the locales directory.
      - Identifying missing translation terms in English.
      - Merging missing terms into the English translation file.
      - Filling out missing terms in other language files based on the English reference.

--- src/components/importLocal/component.tsx
+++ src/components/importLocal/component.tsx
@@ -10,10 +10,7 @@ import { isElectron } from "react-device-detect";
 import { withRouter } from "react-router-dom";
 import BookUtil from "../../utils/file/bookUtil";
 import toast from "react-hot-toast";
-import {
-  CommonTool,
-  ConfigService,
-} from "../../assets/lib/kookit-extra-browser.min";
+import { ConfigService } from "../../assets/lib/kookit-extra-browser.min";
 import CoverUtil from "../../utils/file/coverUtil";
 import {
   calculateFileMD5,
@@ -22,7 +19,6 @@ import {
 } from "../../utils/common";
 import DatabaseService from "../../utils/storage/databaseService";
 import { BookHelper } from "../../assets/lib/kookit-extra-browser.min";
-import SyncService from "../../utils/storage/syncService";
 declare var window: any;
 let clickFilePath = "";
 
---
+++ src/i18n-mobile-script.js
@@ -0,0 +1,75 @@
+//list the folders in ./assets/locales
+const fs = require("fs");
+const path = require("path");
+const localesPath = path.join(__dirname, "./assets/locales");
+const folders = fs.readdirSync(localesPath);
+let resources = [];
+for (let index = 0; index < folders.length; index++) {
+  const folder = folders[index];
+  resources.push(`./assets/locales/${folder}/translation.json`);
+}
+console.log(resources);
+
+// find the missing terms in the english
+const zhdataRaw = fs.readFileSync(
+  path.join(__dirname, "./assets/locales/zh-CN/translation.json"),
+  "utf-8"
+);
+const enDataRaw = fs.readFileSync(
+  path.join(__dirname, "./assets/locales/en/translation.json"),
+  "utf-8"
+);
+//find the missing terms in the target language
+// Step1
+const zhData = JSON.parse(zhdataRaw);
+const enData = JSON.parse(enDataRaw);
+let missingTerms = {};
+for (let index = 0; index < Object.keys(zhData).length; index++) {
+  const term = Object.keys(zhData)[index];
+  if (Object.keys(enData).indexOf(term) === -1) {
+    console.log(zhData[term]);
+    missingTerms[term] = term;
+  }
+}
+console.log(JSON.stringify(missingTerms));
+console.log(Object.keys(zhData).length, Object.keys(enData).length);
+
+// Step2: copy the missing terms to the english file
+const mergedObj = Object.assign({}, enData, missingTerms);
+fs.writeFileSync(
+  path.join(__dirname, "./assets/locales/en/translation.json"),
+  JSON.stringify(mergedObj, null, 2)
+);
+
+// Step3:
+// fill out the rest of the terms in the target language
+const data = fs.readFileSync(
+  path.join(__dirname, "./assets/locales/en/translation.json"),
+  "utf-8"
+);
+const referData = JSON.parse(data);
+for (let index = 0; index < resources.length; index++) {
+  try {
+    const resource = resources[index];
+    console.log(path.join(__dirname, resource));
+    const data = fs.readFileSync(path.join(__dirname, resource), "utf-8");
+    const targetData = JSON.parse(data);
+    let missingTerms = {};
+    for (let index = 0; index < Object.keys(referData).length; index++) {
+      const term = Object.keys(referData)[index];
+      if (Object.keys(targetData).indexOf(term) === -1) {
+        console.log(referData[term]);
+        missingTerms[term] = referData[term];
+      }
+    }
+    // console.log(missingTerms);
+    const mergedObj = Object.assign({}, targetData, missingTerms);
+
+    fs.writeFileSync(
+      path.join(__dirname, resource),
+      JSON.stringify(mergedObj, null, 2)
+    );
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+}
