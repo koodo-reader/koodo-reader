4b3221832106c3c0abdf5286cdbc2995f1b81313
troyeguo <13820674+troyeguo@users.noreply.github.com>
2026-04-07 08:59:10 +0800

    feat(i18n): add script for managing translation files and patching nan.h
    - Implemented a script to list locale folders and identify missing translation terms in English.
    - Added functionality to copy missing terms from Chinese translations to English.
    - Enhanced the script to fill in missing terms for other languages based on the updated English file.
    - Created a patch script for nan.h to comment out the inclusion of nan_scriptorigin.h for cross-platform compatibility.

--- scripts/i18n-script.js
+++ scripts/i18n-script.js
@@ -1,22 +1,22 @@
 //list the folders in ./assets/locales
 const fs = require("fs");
 const path = require("path");
-const localesPath = path.join(__dirname, "./assets/locales");
+const localesPath = path.join(__dirname, "../src/assets/locales");
 const folders = fs.readdirSync(localesPath);
 let resources = [];
 for (let index = 0; index < folders.length; index++) {
   const folder = folders[index];
-  resources.push(`./assets/locales/${folder}/translation.json`);
+  resources.push(`../src/assets/locales/${folder}/translation.json`);
 }
 console.log(resources);
 
 // find the missing terms in the english
 const zhdataRaw = fs.readFileSync(
-  path.join(__dirname, "./assets/locales/zh-CN/translation.json"),
+  path.join(__dirname, "../src/assets/locales/zh-CN/translation.json"),
   "utf-8"
 );
 const enDataRaw = fs.readFileSync(
-  path.join(__dirname, "./assets/locales/en/translation.json"),
+  path.join(__dirname, "../src/assets/locales/en/translation.json"),
   "utf-8"
 );
 //find the missing terms in the target language
@@ -37,14 +37,14 @@ console.log(Object.keys(zhData).length, Object.keys(enData).length);
 // Step2: copy the missing terms to the english file
 const mergedObj = Object.assign({}, enData, missingTerms);
 fs.writeFileSync(
-  path.join(__dirname, "./assets/locales/en/translation.json"),
+  path.join(__dirname, "../src/assets/locales/en/translation.json"),
   JSON.stringify(mergedObj, null, 2)
 );
 
 // Step3:
 // fill out the rest of the terms in the target language
 const data = fs.readFileSync(
-  path.join(__dirname, "./assets/locales/en/translation.json"),
+  path.join(__dirname, "../src/assets/locales/en/translation.json"),
   "utf-8"
 );
 const referData = JSON.parse(data);
