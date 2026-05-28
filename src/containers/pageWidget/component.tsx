a7e26708098d010585b1dc26a29e3491db42b0bc
troyeguo <13820674+troyeguo@users.noreply.github.com>
2026-05-28 08:32:21 +0800

    fix: change jump-return-button-container position to fixed and adjust z-index

--- src/containers/pageWidget/component.tsx
+++ src/containers/pageWidget/component.tsx
@@ -179,135 +179,139 @@ class PageWidget extends React.Component<PageWidgetProps, PageWidgetState> {
 
   render() {
     return (
-      <div
-        className="background"
-        style={{
-          color: ConfigService.getReaderConfig("textColor")
-            ? ConfigService.getReaderConfig("textColor")
-            : "",
-          width:
-            !this.props.isNavLocked && !this.props.isSettingLocked
-              ? "100%"
-              : this.props.isNavLocked && this.props.isSettingLocked
-                ? "calc(100% - 600px)"
-                : "calc(100% - 300px)",
-          left: !this.props.isNavLocked ? "0" : "300px",
-          right: !this.props.isSettingLocked ? "0" : "300px",
-          backgroundColor: this.props.backgroundColor,
-          filter: `brightness(${
-            ConfigService.getReaderConfig("brightness") || 1
-          }) invert(${
-            ConfigService.getReaderConfig("isInvert") === "yes" ? 1 : 0
-          })`,
-        }}
-      >
-        <div className="header-container">
-          {!this.props.isHideHeader && this.props.currentChapter + "" && (
-            <p
-              className="header-chapter-name"
-              style={
-                this.state.isSingle
-                  ? {
-                      left: `calc(50vw - 
-                      270px)`,
-                    }
-                  : {}
-              }
-            >
-              {this.props.currentChapter}
-            </p>
-          )}
-          {!this.props.isHideHeader &&
-            this.props.currentChapter + "" &&
-            !this.state.isSingle && (
+      <>
+        <div
+          className="background"
+          style={{
+            color: ConfigService.getReaderConfig("textColor")
+              ? ConfigService.getReaderConfig("textColor")
+              : "",
+            width:
+              !this.props.isNavLocked && !this.props.isSettingLocked
+                ? "100%"
+                : this.props.isNavLocked && this.props.isSettingLocked
+                  ? "calc(100% - 600px)"
+                  : "calc(100% - 300px)",
+            left: !this.props.isNavLocked ? "0" : "300px",
+            right: !this.props.isSettingLocked ? "0" : "300px",
+            backgroundColor: this.props.backgroundColor,
+            filter: `brightness(${
+              ConfigService.getReaderConfig("brightness") || 1
+            }) invert(${
+              ConfigService.getReaderConfig("isInvert") === "yes" ? 1 : 0
+            })`,
+          }}
+        >
+          <div className="header-container">
+            {!this.props.isHideHeader && this.props.currentChapter + "" && (
               <p
-                className="header-book-name"
+                className="header-chapter-name"
                 style={
                   this.state.isSingle
                     ? {
-                        right: `calc(50vw - 
+                        left: `calc(50vw - 
                       270px)`,
                       }
                     : {}
                 }
               >
-                {this.props.currentBook.name}
+                {this.props.currentChapter}
               </p>
             )}
-          {!this.props.isHideHeader && (
-            <>
-              <span className="footer-time">
-                {this.state.currentTime}
-                {this.state.percentage
-                  ? "  " +
-                    (parseFloat(this.state.percentage) * 100).toFixed(2) +
-                    "%"
-                  : ""}
-              </span>
-            </>
-          )}
-        </div>
-        <div className="footer-container">
-          {!this.props.isHideFooter && this.state.prevPage > 0 && (
-            <p
-              className="background-page-left"
-              style={
-                this.state.isSingle
-                  ? {
-                      left: `calc(50vw - 
+            {!this.props.isHideHeader &&
+              this.props.currentChapter + "" &&
+              !this.state.isSingle && (
+                <p
+                  className="header-book-name"
+                  style={
+                    this.state.isSingle
+                      ? {
+                          right: `calc(50vw - 
                       270px)`,
-                    }
-                  : {}
-              }
-            >
-              <Trans i18nKey="Book page" count={this.state.prevPage}>
-                Page
-                {{
-                  count: this.state.prevPage,
-                }}
-              </Trans>
-            </p>
-          )}
-          {!this.props.isHideFooter &&
-            this.state.nextPage > 0 &&
-            !this.state.isSingle && (
-              <p className="background-page-right">
-                <Trans i18nKey="Book page" count={this.state.nextPage}>
+                        }
+                      : {}
+                  }
+                >
+                  {this.props.currentBook.name}
+                </p>
+              )}
+            {!this.props.isHideHeader && (
+              <>
+                <span className="footer-time">
+                  {this.state.currentTime}
+                  {this.state.percentage
+                    ? "  " +
+                      (parseFloat(this.state.percentage) * 100).toFixed(2) +
+                      "%"
+                    : ""}
+                </span>
+              </>
+            )}
+          </div>
+          <div className="footer-container">
+            {!this.props.isHideFooter && this.state.prevPage > 0 && (
+              <p
+                className="background-page-left"
+                style={
+                  this.state.isSingle
+                    ? {
+                        left: `calc(50vw - 
+                      270px)`,
+                      }
+                    : {}
+                }
+              >
+                <Trans i18nKey="Book page" count={this.state.prevPage}>
                   Page
                   {{
-                    count: this.state.nextPage,
+                    count: this.state.prevPage,
                   }}
                 </Trans>
               </p>
             )}
-        </div>
-        <>
-          {this.props.isShowBookmark ? <div className="bookmark"></div> : null}
-        </>
-        {this.props.isShowPageBorder && (
+            {!this.props.isHideFooter &&
+              this.state.nextPage > 0 &&
+              !this.state.isSingle && (
+                <p className="background-page-right">
+                  <Trans i18nKey="Book page" count={this.state.nextPage}>
+                    Page
+                    {{
+                      count: this.state.nextPage,
+                    }}
+                  </Trans>
+                </p>
+              )}
+          </div>
           <>
-            <div className="page-border"></div>
-            <div className="inner-page-border"></div>
-            <div className="page-border-header-line"></div>
-            <div className="page-border-footer-line"></div>
-            {!this.state.isSingle && (
-              <div
-                className="page-border-center-line"
-                style={
-                  this.props.textOrientation === "vertical"
-                    ? {
-                        top: "50%",
-                        height: "1px",
-                        width: "calc(100% - 30px)",
-                        left: "15px",
-                        right: "15px",
-                      }
-                    : {}
-                }
-              ></div>
-            )}
+            {this.props.isShowBookmark ? (
+              <div className="bookmark"></div>
+            ) : null}
           </>
-        )}
+          {this.props.isShowPageBorder && (
+            <>
+              <div className="page-border"></div>
+              <div className="inner-page-border"></div>
+              <div className="page-border-header-line"></div>
+              <div className="page-border-footer-line"></div>
+              {!this.state.isSingle && (
+                <div
+                  className="page-border-center-line"
+                  style={
+                    this.props.textOrientation === "vertical"
+                      ? {
+                          top: "50%",
+                          height: "1px",
+                          width: "calc(100% - 30px)",
+                          left: "15px",
+                          right: "15px",
+                        }
+                      : {}
+                  }
+                ></div>
+              )}
+            </>
+          )}
+        </div>
         {this.props.jumpPosition && (
           <div className="jump-return-button-container">
             <button
@@ -326,7 +330,7 @@ class PageWidget extends React.Component<PageWidgetProps, PageWidgetState> {
             </button>
           </div>
         )}
-      </div>
+      </>
     );
   }
 }
