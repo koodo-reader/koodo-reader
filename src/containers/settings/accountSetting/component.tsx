912ab76b404a8f544c4efcdd56b6acbbb60c4e6d
troyeguo <13820674+troyeguo@users.noreply.github.com>
2026-08-15 10:59:13 +0800

    feat: add "Learn more" link for server region selection in account settings fix: remove box-shadow from background and font setting styles

--- src/containers/settings/accountSetting/component.tsx
+++ src/containers/settings/accountSetting/component.tsx
@@ -626,9 +626,34 @@ class AccountSetting extends React.Component<
           </div>
         )}
         <div className="setting-dialog-new-title">
-          <Trans>
-            {this.props.isAuthed ? "Server region" : "Select server region"}
-          </Trans>
+          <div>
+            <Trans>
+              {this.props.isAuthed ? "Server region" : "Select server region"}
+            </Trans>
+            {ConfigService.getReaderConfig("lang").startsWith("zh") && (
+              <span
+                style={{
+                  textDecoration: "underline",
+                  marginLeft: "10px",
+                  cursor: "pointer",
+                  opacity: 0.7,
+                  fontSize: "13px",
+                  lineHeight: "16px",
+                }}
+                onClick={() => {
+                  openInBrowser(
+                    getWebsiteUrl() +
+                      (ConfigService.getReaderConfig("lang").startsWith("zh")
+                        ? "/zh"
+                        : "/en") +
+                      "/server-region"
+                  );
+                }}
+              >
+                <Trans>Learn more</Trans>
+              </span>
+            )}
+          </div>
           {this.props.isAuthed ? (
             <div
               className="lang-setting-option"
